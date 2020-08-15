24eac60606c6ad0093bed86aa866efe0354e1427
dia006 <58293773+dia006@users.noreply.github.com>
2020-08-15 12:05:54 +0200

    Uniformed

--- db/000-LeanMaint.sql
+++ db/000-LeanMaint.sql
@@ -1,195 +1,238 @@
 USE [LeanMaint]
 GO
-ALTER DATABASE SCOPED CONFIGURATION SET IDENTITY_CACHE = ON;
+/****** Object:  Schema [Accountancy]    Script Date: 8/15/2020 3:04:30 AM ******/
+CREATE SCHEMA [Accountancy]
 GO
-ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
+/****** Object:  Schema [Config]    Script Date: 8/15/2020 3:04:30 AM ******/
+CREATE SCHEMA [Config]
 GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET LEGACY_CARDINALITY_ESTIMATION = PRIMARY;
+/****** Object:  Schema [Device]    Script Date: 8/15/2020 3:04:30 AM ******/
+CREATE SCHEMA [Device]
 GO
-ALTER DATABASE SCOPED CONFIGURATION SET MAXDOP = 0;
+/****** Object:  Schema [Maintenance]    Script Date: 8/15/2020 3:04:30 AM ******/
+CREATE SCHEMA [Maintenance]
 GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET MAXDOP = PRIMARY;
-GO
-ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = ON;
-GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET PARAMETER_SNIFFING = PRIMARY;
-GO
-ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
-GO
-ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET QUERY_OPTIMIZER_HOTFIXES = PRIMARY;
-GO
-USE [LeanMaint]
-GO
-/****** Object:  Table [dbo].[Anagrafico Impianti]    Script Date: 25/07/2020 21:31:27 ******/
+/****** Object:  Table [Accountancy].[Billings]    Script Date: 8/15/2020 3:04:30 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Anagrafico Impianti](
-	[ID_PLant] [varchar](30) NOT NULL,
-	[Desc_Plant] [varchar](200) NULL,
-	[DT_Install_Plant] [datetime] NOT NULL,
-	[Costo_Plant] [int] NULL,
-	[Azienda] [varchar](100) NULL,
-	[Divisione] [varchar](100) NULL,
-	[CDC_Plant] [varchar](20) NULL,
-	[Tipo_Plant] [varchar](20) NULL,
-	[DT_Lastmaint] [int] NULL,
-	[DT_Nextmaint] [int] NULL,
-	[Esito_maint] [varchar](10) NULL,
-	[Grado_risk_plant] [int] NULL,
-	[Stato] [varchar](3) NULL,
+CREATE TABLE [Accountancy].[Billings](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[ID_Operator] [int] NULL,
+	[Activation] [datetime] NOT NULL,
+	[End] [datetime] NULL,
+	[HourlyCost] [decimal](9, 4) NULL,
+	[Year] [int] NULL,
+	[Month] [int] NULL,
+	[StartDay] [datetime] NULL,
+	[EndDay] [datetime] NULL,
+	[Value] [decimal](19, 4) NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[ID_PLant] ASC,
-	[DT_Install_Plant] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Anagrafico Interventi]    Script Date: 25/07/2020 21:31:28 ******/
+/****** Object:  Table [Config].[Branches]    Script Date: 8/15/2020 3:04:30 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Anagrafico Interventi](
-	[ID_PLant] [varchar](30) NOT NULL,
-	[Desc_Inter] [varchar](200) NULL,
-	[DT_Chiamata] [datetime] NULL,
-	[Tarif_Inter] [int] NULL,
-	[Tipo_Risorsa] [varchar](100) NULL,
-	[Desc_Risorsa] [varchar](100) NULL,
-	[CDC_Risorsa] [varchar](20) NULL,
-	[Tipo_Plant] [varchar](20) NULL,
-	[DT_Conf_Int] [int] NULL,
-	[DT_Eff_Int] [int] NULL,
-	[DT_Ultimo_Guasto] [int] NULL,
-	[DT_Ultima_Riparazione] [int] NULL,
-	[Esito_maint] [varchar](10) NULL,
-	[Stato] [varchar](3) NULL,
-	[ID_Inter] [varchar](50) NOT NULL,
-	[ID_Persona] [varchar](100) NULL,
-	[ORE_Inter] [int] NULL,
-	[Costo_Inter] [int] NULL,
-	[Probabil] [int] NULL,
-	[Gravity] [int] NULL,
-	[Rilevabil] [int] NULL,
+CREATE TABLE [Config].[Branches](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Description] [nvarchar](200) NOT NULL,
+	[ID_Company] [int] NOT NULL,
+ CONSTRAINT [PK_ConfigBranch] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [Config].[Companies]    Script Date: 8/15/2020 3:04:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Config].[Companies](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](200) NULL,
+ CONSTRAINT [PK_ConfigCompany] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [Config].[CostCenters]    Script Date: 8/15/2020 3:04:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Config].[CostCenters](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Description] [nvarchar](200) NOT NULL,
+	[ID_Company] [int] NOT NULL,
+ CONSTRAINT [PK_ConfigCostCenter] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [Config].[Countries]    Script Date: 8/15/2020 3:04:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Config].[Countries](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Description] [nvarchar](200) NOT NULL,
+ CONSTRAINT [PK_ConfigCountries] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [Config].[Departments]    Script Date: 8/15/2020 3:04:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Config].[Departments](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Description] [nvarchar](200) NOT NULL,
+	[ID_Branch] [int] NOT NULL,
+ CONSTRAINT [PK_ConfigDivision] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [Config].[FailureStatuses]    Script Date: 8/15/2020 3:04:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Config].[FailureStatuses](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](200) NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[ID_PLant] ASC,
-	[ID_Inter] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Anagrafico_Fornitori]    Script Date: 25/07/2020 21:31:28 ******/
+/****** Object:  Table [Config].[ObjStatuses]    Script Date: 8/15/2020 3:04:30 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Anagrafico_Fornitori](
-	[ID_PLant] [varchar](30) NOT NULL,
-	[ID_Fornitore] [varchar](100) NOT NULL,
-	[Desc_Fornitore] [varchar](300) NULL,
-	[Indirizzo1] [varchar](200) NULL,
-	[Indirizzo2] [varchar](200) NULL,
+CREATE TABLE [Config].[ObjStatuses](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](200) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [Config].[Operators]    Script Date: 8/15/2020 3:04:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Config].[Operators](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[MiddleName] [nvarchar](50) NULL,
+	[LastName] [nvarchar](50) NULL,
+	[Description] [nvarchar](200) NULL,
+	[Coded] [int] NULL,
+	[ID_Supplier] [int] NULL,
 	[CDC_Riferimento] [varchar](20) NULL,
-	[Costo_Orario] [int] NULL,
-	[CityFrazione] [varchar](200) NULL,
-	[Nazione] [varchar](100) NULL,
-	[CAP_Forn] [varchar](7) NULL,
-	[DT_Valid_Forn] [datetime] NULL,
-	[DT_Last_Inter] [datetime] NULL,
-	[Stato] [varchar](3) NULL,
-PRIMARY KEY CLUSTERED 
+	[HourlyCost] [decimal](5, 4) NULL,
+	[ID_ObjStatus] [int] NULL,
+ CONSTRAINT [PK_HR_Manut] PRIMARY KEY CLUSTERED 
 (
-	[ID_PLant] ASC,
-	[ID_Fornitore] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Autorizzazioni]    Script Date: 25/07/2020 21:31:28 ******/
+/****** Object:  Table [Config].[OperatorsPermission]    Script Date: 8/15/2020 3:04:30 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Autorizzazioni](
-	[ID_User] [varchar](50) NOT NULL,
-	[DT_Ins_User] [datetime] NULL,
+CREATE TABLE [Config].[OperatorsPermission](
+	[ID_Operator] [int] NOT NULL,
 	[LVL_User] [int] NULL,
 	[Auth_Code] [varchar](15) NULL,
-	[DT_End_Auth] [datetime] NULL,
+	[Started] [datetime] NULL,
+	[Ended] [datetime] NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[ID_User] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[ID_Operator] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[BILLING]    Script Date: 25/07/2020 21:31:28 ******/
+/****** Object:  Table [Config].[Plants]    Script Date: 8/15/2020 3:04:30 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[BILLING](
-	[ID_USER] [varchar](50) NOT NULL,
-	[DESC_USER] [varchar](300) NULL,
-	[DT_ATTIVAZIONE] [datetime] NOT NULL,
-	[DT_TERMINE] [datetime] NULL,
-	[TARIFFA] [numeric](21, 0) NULL,
-	[ANNO] [varchar](4) NULL,
-	[MESE] [varchar](20) NULL,
-	[GIORNO_INIZIO] [numeric](2, 0) NULL,
-	[GIORNO_FINE] [numeric](2, 0) NULL,
-	[VALORE] [numeric](21, 0) NULL,
-PRIMARY KEY CLUSTERED 
+CREATE TABLE [Config].[Plants](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](200) NULL,
+	[ID_ObjStatus] [int] NULL,
+	[ID_PlantType] [int] NULL,
+	[ID_PlantRisk] [int] NULL,
+	[ID_Department] [int] NOT NULL,
+	[ID_CostCenter] [int] NULL,
+	[Created] [datetime] NOT NULL,
+ CONSTRAINT [PK_Anagrafico Impianti] PRIMARY KEY CLUSTERED 
 (
-	[ID_USER] ASC,
-	[DT_ATTIVAZIONE] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[GUASTI]    Script Date: 25/07/2020 21:31:28 ******/
+/****** Object:  Table [Config].[Suppliers]    Script Date: 8/15/2020 3:04:30 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[GUASTI](
-	[ID_Guasto] [varchar](30) NOT NULL,
-	[Desc_Guasto] [varchar](300) NULL,
-	[Classe_Guasto] [varchar](1) NULL,
-	[Stato] [varchar](3) NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[ID_Guasto] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[HR_Manut]    Script Date: 25/07/2020 21:31:28 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[HR_Manut](
+CREATE TABLE [Config].[Suppliers](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](200) NULL,
 	[ID_PLant] [varchar](30) NOT NULL,
-	[ID_Persona] [varchar](100) NOT NULL,
-	[Desc_Persona] [varchar](300) NULL,
-	[Codificato] [int] NULL,
-	[ID_Fornitore] [varchar](100) NULL,
-	[DT_Ultima_Att] [datetime] NULL,
-	[CDC_Riferimento] [varchar](20) NULL,
-	[Costo_Orario] [int] NULL,
-	[Stato] [varchar](3) NULL,
-PRIMARY KEY CLUSTERED 
+	[Address1] [varchar](200) NULL,
+	[Address2] [varchar](200) NULL,
+	[ID_CostCenter] [int] NULL,
+	[HourlyCost] [decimal](5, 4) NULL,
+	[Town] [varchar](200) NULL,
+	[Zip] [varchar](7) NULL,
+	[ID_Country] [int] NULL,
+	[ValidFrom] [datetime] NULL,
+	[ID_ObjStatus] [int] NULL,
+ CONSTRAINT [PK_ConfigSuppliers] PRIMARY KEY CLUSTERED 
 (
-	[ID_PLant] ASC,
-	[ID_Persona] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Interfaccie]    Script Date: 25/07/2020 21:31:28 ******/
+/****** Object:  Table [Device].[Interfaces]    Script Date: 8/15/2020 3:04:30 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Interfaccie](
+CREATE TABLE [Device].[Interfaces](
 	[SISTEMA] [varchar](50) NOT NULL,
 	[DT_Attivazione] [datetime] NULL,
 	[Dt_disattivazione] [datetime] NULL,
@@ -205,63 +248,124 @@ PRIMARY KEY CLUSTERED
 (
 	[SISTEMA] ASC,
 	[Auth_Code] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Materiali_Manut]    Script Date: 25/07/2020 21:31:28 ******/
+/****** Object:  Table [Maintenance].[Failures]    Script Date: 8/15/2020 3:04:30 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Materiali_Manut](
-	[ID_PLant] [varchar](30) NOT NULL,
-	[ID_Mater] [varchar](100) NOT NULL,
-	[Desc_Mater] [varchar](300) NULL,
-	[Codificato] [int] NULL,
-	[ID_Fornitore] [varchar](100) NULL,
-	[DT_Uso _Mater] [datetime] NULL,
-	[CDC_Riferimento] [varchar](20) NULL,
-	[Costo_Mater] [int] NULL,
-	[Stato] [varchar](3) NULL,
+CREATE TABLE [Maintenance].[Failures](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](200) NULL,
+	[ID_FailureClass] [int] NULL,
+	[ID_ObjStatus] [int] NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[ID_PLant] ASC,
-	[ID_Mater] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[RIPARAZIONI]    Script Date: 25/07/2020 21:31:28 ******/
+/****** Object:  Table [Maintenance].[Interventions]    Script Date: 8/15/2020 3:04:30 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[RIPARAZIONI](
-	[ID_Ripar] [varchar](30) NOT NULL,
-	[Desc_Ripar] [varchar](300) NULL,
-	[Classe_Ripar] [varchar](1) NULL,
-	[Stato] [varchar](3) NULL,
+CREATE TABLE [Maintenance].[Interventions](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](200) NULL,
+	[ID_Operator] [int] NULL,
+	[ID_Plant] [int] NULL,
+	[RequestedAt] [datetime] NULL,
+	[Tarif_Inter] [int] NULL,
+	[Tipo_Risorsa] [varchar](100) NULL,
+	[Desc_Risorsa] [varchar](100) NULL,
+	[CDC_Risorsa] [varchar](20) NULL,
+	[Tipo_Plant] [varchar](20) NULL,
+	[DT_Conf_Int] [int] NULL,
+	[DT_Eff_Int] [int] NULL,
+	[DT_Ultimo_Guasto] [int] NULL,
+	[DT_Ultima_Riparazione] [int] NULL,
+	[ID_Outcome] [int] NULL,
+	[ID_ObjStatus] [int] NULL,
+	[Hours] [int] NULL,
+	[TotalCost] [int] NULL,
+	[Probability] [int] NULL,
+	[Gravity] [int] NULL,
+	[Rilevability] [int] NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[ID_Ripar] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[STATI]    Script Date: 25/07/2020 21:31:28 ******/
+/****** Object:  Table [Maintenance].[Materials]    Script Date: 8/15/2020 3:04:30 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[STATI](
-	[ID_STATO] [varchar](3) NOT NULL,
-	[Desc_Guasto] [varchar](300) NULL,
-	[Stato] [varchar](3) NULL,
+CREATE TABLE [Maintenance].[Materials](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](200) NULL,
+	[ID_Plant] [int] NULL,
+	[Coded] [int] NULL,
+	[ID_Supplier] [varchar](100) NULL,
+	[UsedFrom] [datetime] NULL,
+	[ID_CostCenter] [varchar](20) NULL,
+	[Cost] [decimal](19, 4) NULL,
+	[ID_ObjStatus] [int] NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[ID_STATO] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-USE [master]
+/****** Object:  Table [Maintenance].[Outcomes]    Script Date: 8/15/2020 3:04:30 AM ******/
+SET ANSI_NULLS ON
 GO
-ALTER DATABASE [LeanMaint] SET  READ_WRITE 
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Maintenance].[Outcomes](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](200) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [Maintenance].[RepairClasses]    Script Date: 8/15/2020 3:04:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Maintenance].[RepairClasses](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](200) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [Maintenance].[Repairs]    Script Date: 8/15/2020 3:04:30 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [Maintenance].[Repairs](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](200) NULL,
+	[ID_RepairClass] [int] NULL,
+	[ID_ObjStatus] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
